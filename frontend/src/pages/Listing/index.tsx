578668ed80ba6bbe2c8c10bc9c2995eb8e0fe07b
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-14 10:50:11 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,41 +1,50 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-  // FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-          console.log(response.data);
-        });
+  const [pageNumber, setPageNumber] = useState(0);
 
-    return(
-      <>
-        <Pagination/>
-        <div className="container">
-          <div className="row">
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=3`)
+      .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPageNumber(data.number);
+      });
 
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-              <MovieCard /> 
-            </div>
+  })
 
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-              <MovieCard /> 
-            </div>
+  return (
+    <>
+    <p>{pageNumber}</p>
+      <Pagination />
+      <div className="container">
+        <div className="row">
 
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-              <MovieCard /> 
-            </div>
-
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-              <MovieCard /> 
-            </div>
-
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+            <MovieCard />
           </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+            <MovieCard />
+          </div>
+
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Listing;
